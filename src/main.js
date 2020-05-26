f5fa14249a74ab9a96e678cd0fe4ca2adbd0f434
YAYO严 <15720826383@163.com>
2020-05-26 15:51:32 +0800

    安装配置element-ui和babel-polyfill

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import store from './store'
 
 Vue.config.productionTip = false;
 
+//安装使用elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//修改elementUI的默认样式
+// import './element-var.scss';
+Vue.use(ElementUI);
+//设置elementUI的全局size
+Vue.prototype.$ELEMENT = {size: 'mini'};
+
+//导入babel-polyfill，解决ie9和一些低版本的高级浏览器对es6新语法并不支持
+import "babel-polyfill";
+
 new Vue({
   router,
   store,
